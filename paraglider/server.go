e8a5198a2967ea5408a145a7e5f577c6ed2f7685
Einar Budsted <budsted@hotmail.com>
2018-10-29 11:48:40 +0100

    hello world

---
+++ paraglider/server.go
@@ -0,0 +1,29 @@
+package paragliding
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Server struct {
+}
+
+func (server *Server) Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+func (server *Server) Start() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT not set")
+		return
+	}
+
+	http.HandleFunc("/", server.Hello)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		panic(err)
+	}
+}
